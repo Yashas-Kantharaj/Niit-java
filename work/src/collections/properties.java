e8c784c66b9073425cfc80d1a8129f22203547e2
yasha <yasha@Yashas>
2021-04-05 14:32:23 +0530

    init

---
+++ work/src/collections/properties.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+public class properties {
+	
+	public static void main(String[] args)throws Exception{  
+	    FileReader reader=new FileReader("C:\\Users\\yasha\\git\\Niit-java\\work\\src\\collections\\db.properties");  
+	      
+	    Properties p=new Properties();  
+	    p.load(reader);  
+	      
+	    System.out.println(p.getProperty("user"));  
+	    System.out.println(p.getProperty("password"));  
+	}  
+}  
