7b84c9c37d8a7d322610a3289368fcc3109e2f11
yasha <yasha@Yashas>
2021-04-06 14:32:32 +0530

    2

--- work/src/generic/testob.java
+++ work/src/generic/testob.java
@@ -8,5 +8,12 @@ public class testob {
 		gen<Integer> g2 = new gen<Integer>(20);
 		g2.show();
 		System.out.println(g2.getob());
+		
+		gen<?> g3 = new gen<Integer>(new Integer(250));
+		gen<? super String >g4 = new gen<Object>("hello");
+		g3.show();
+		g4.show();
+		System.out.println(g3.getob());
+		System.out.println(g4.getob());
 	}
 }
