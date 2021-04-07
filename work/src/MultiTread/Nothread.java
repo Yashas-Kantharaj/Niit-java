feba77b9784817d1977c8a793ef9a4ce33769fcb
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-07 16:01:39 +0530

    edited

--- work/src/MultiTread/Nothread.java
+++ work/src/MultiTread/Nothread.java
@@ -8,7 +8,7 @@ public class Nothread extends Thread {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			}
 		
