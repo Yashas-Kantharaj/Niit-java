952811dd289df8c1483ffae3cbd700e4ac372e14
yasha <yasha@Yashas>
2021-04-07 11:29:32 +0530

    instance

---
+++ work/src/serialization/Instance.java
@@ -0,0 +1,23 @@
+package serialization;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Instance {
+	public static void main(String[] args) throws Exception{
+		FileInputStream fis = new FileInputStream("animals.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Object o = ois.readObject();
+		ois.close();
+		
+		if(o instanceof Cat) {
+			Cat c1 = (Cat)o;
+		}
+		else if(o instanceof Dog) {
+			Dog d1 = (Dog)o;
+		}
+		else 		if(o instanceof Rat) {
+			Rat r1 = (Rat)o;
+		}
+
+	}
+}
