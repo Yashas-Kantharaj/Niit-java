5efd0a2d29543af19cd4fbaba30b189c1a9e1365
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-08 10:32:04 +0530

    lamda

---
+++ work/src/Lamba/Lamda1.java
@@ -0,0 +1,16 @@
+package Lamba;
+import java.util.*;
+import java.util.function.Function;
+public class Lamda1 {
+	public static void main(String[] args) {
+		int sq =squareIt(5);
+		System.out.println(sq);
+		
+		Function<Integer,Integer> f=n-> n*n;
+		System.out.println("Lamda function="+f.apply(4));
+ 	}
+
+	private static int squareIt(int n) {	
+		return n*n;
+	}
+}
