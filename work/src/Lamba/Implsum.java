3d4831d54b9fc79e8fd56588100e0915238f41d9
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-08 11:33:23 +0530

    LB4FI

---
+++ work/src/Lamba/Implsum.java
@@ -0,0 +1,10 @@
+package Lamba;
+
+public class Implsum implements FunctionalInterface{
+	@Override
+	public int Isum(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
---
+++ work/src/Lamba/LB4FuncInterface.java
@@ -0,0 +1,9 @@
+package Lamba;
+
+public class LB4FuncInterface {
+	public static void main(String[] args) {
+		Implsum isum = new Implsum();
+		System.out.println(isum.Isum(20,20));
+	}
+
+}
