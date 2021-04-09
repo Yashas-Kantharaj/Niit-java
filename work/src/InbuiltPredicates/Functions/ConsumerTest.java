41da63405f7beeed65cf891a86bcad7173d82094
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-09 12:37:36 +0530

    consumer 1

--- work/src/InbuiltPredicates/Functions/ConsumerTest.java
+++ work/src/InbuiltPredicates/Functions/ConsumerTest.java
@@ -2,13 +2,18 @@ package InbuiltPredicates.Functions;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class ConsumerTest {
 	static ArrayList<JavaStudent> js= TestJavaStudent.popstudents();
 	public static void main(String[] args) {
+		Predicate<JavaStudent> p = s -> s.marks >=65;
+		
 		Consumer<JavaStudent> c= s ->  System.out.println(s.name);
+		System.out.println("Student who are more than 65 :");
 		for(JavaStudent s : js) {
-			c.accept(s);
+			if(p.test(s))
+				c.accept(s);
 			
 		}
 	}
