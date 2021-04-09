93233f7c9c0bc90e5bf46cd8fea20dce4683ca30
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-09 12:05:54 +0530

    Functions

--- work/src/InbuiltPredicates/Functions/TestFunctions.java
+++ work/src/InbuiltPredicates/Functions/TestFunctions.java
@@ -5,7 +5,8 @@ import java.util.function.Function;
 public class TestFunctions {
 	public static void main(String[] args) {
 		Function<Integer,Integer>f = i-> i*i;
-		System.out.println(f.apply(5));
+		System.out.println(f.apply(4));
+
 		
 	}
 }
